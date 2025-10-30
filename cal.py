452a5dc3a177f8c37b2bcb5bde56e5abcb62652d
rishitajadav11-lgtm <rishitajadav11@gmail.com>
2025-10-30 20:08:59 +0530

    Added my changes

--- cal.py
+++ cal.py
@@ -3,7 +3,7 @@ n = int(input("Enter Your Total Subject's :- "))
 total =0 
 
 for i in range(1, n+1):
-    mark = float(input(f"Enter Your Marks of Subject {i} :-"))
+    mark = float(input(f"Enter sfsfsfYour Marks of Subject {i} :-"))
     total += mark
 
 percentage = total / n
